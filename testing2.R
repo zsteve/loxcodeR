ab47c94c27c1c47279f5bc98aa91976768e30302
zsteve <stephenz@student.unimelb.edu.au>
2019-08-07 17:44:44 +1000

    added code for testing optimisation

--- testing2.R
+++ testing2.R
@@ -1,4 +1,5 @@
 library(loxcoder)
+library(plotly)
 
 loxcoder::load_origin_distmaps('/run/media/zsteve/ssd_ext/loxcode/maps/origin')
 loxcoder::load_pair_distmaps('/run/media/zsteve/ssd_ext/loxcode/maps/pair')
@@ -36,20 +37,76 @@ loxcoder::min_flip_dist(c = t$code, size = t$size, v = t$is_valid)
 
 ## Markov stuff
 library(optimbase)
-T = lapply(list(c(13, 1/66),
-             c(9, 1/28),
-             c(7, 1/15),
-             c(5, 1/6),
-             c(3, 1)), function(x) x[2]*ones(x[1]+1))
+library(nloptr)
 
-u <- loxcoder::run_markov(4, T)
+p_inv = function(x) 1
+p_exc = function(x) 1
+p_non = 0
+R = lapply(c(13, 9, 7, 5, 3), function(x) get_tr_mat(x, p_inv, p_exc, p_non))
+
+u <- loxcoder::run_markov(4, R)
 lapply(u, function(x) sum(x$p))
 
-v <- filter(t, dist_orig <= 2 & size == 5)
-m <- merge(v, u[[3]], by = 'code', all = TRUE)
-m <- merge(m, u[[2]], by = 'code', all = TRUE)
-m$freq <- m$count/sum(m$count, na.rm = T)
-g <- ggplot(data = m) + geom_beeswarm(aes(x = log10(freq), y = log10(p.x), text = code, color = 'red'), groupOnX = FALSE) +
-  geom_beeswarm(aes(x = log10(freq), y = log10(p.y), text = code, color = 'blue'), groupOnX = FALSE) +
-  geom_abline(slope = 1, intercept = 0)
+# k=4
+# ggplot() + geom_point(aes(x = u[[k]]$p, y = u_old[[k]]$p)) + scale_x_log10() + scale_y_log10() +
+#   geom_abline(slope = 1, intercept = 0)
+
+f <- function(x){
+
+  p_inv <- function(r){
+    if(r == 3){
+      return(x[1]) # 1/100
+    }else{
+      return(x[2]) # 1
+    }
+  }
+  p_exc <- function(r){
+    if(r == 4){
+      return(x[3]) # 5
+    }else{
+      return(x[4]) # 5
+    }
+  }
+  p_non <- 0
+  R = lapply(c(13, 9, 7, 5, 3), function(x) get_tr_mat(x, p_inv, p_exc, p_non))
+  u_new <- loxcoder::run_markov(2, R)
+
+  v <- filter(t, dist_orig <= 2 & size == 5)
+  m <- merge(v, u[[3]], by = 'code', all = TRUE)
+  m <- merge(m, u_new[[3]], by = 'code', all = TRUE)
+  m$freq <- m$count/sum(m$count, na.rm = T)
+#   return(mean((log(m$p.y) - log(m$freq))^2, na.rm = T))
+  return(summary(lm(data = m, freq ~ p.y))$r.squared)
+}
+
+g <- ggplot(data = m) + geom_beeswarm(aes(x = log10(freq), y = log10(p.x), text = code), color = 'red', groupOnX = FALSE) +
+  geom_beeswarm(aes(x = log10(freq), y = log10(p.y), text = code), color = 'green', groupOnX = FALSE) +
+ geom_abline(slope = 1, intercept = 0)
 ggplotly(g)
+
+
+get_tr_mat <- function(n, p_inv, p_exc, p_non){
+  M = zeros(n+1)
+  for(i in 1:(n+1)){
+    for(j in 1:(n+1)){
+      p = 0
+      if(abs(i-j) %% 2 == 1){
+        # inversion
+        if(abs(i-j) < 3){
+          p = p_non
+        }else{
+          p = p_inv(abs(i-j))
+        }
+      }else{
+        if(abs(i-j) < 4){
+          p = p_non
+        }else{
+          p = p_exc(abs(i-j))
+        }
+      }
+      M[i, j] = p
+      M[j, i] = p
+    }
+  }
+  return(M/(0.5*sum(M)))
+}
